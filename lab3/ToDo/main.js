86eecfb17512d97dd6a988162a8498f265d8a34f
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-02-14 23:55:12 +0600

    Update main.js

--- lab3/ToDo/main.js
+++ lab3/ToDo/main.js
@@ -4,6 +4,16 @@ window.addEventListener('load', function() {
 	const list_el = document.querySelector("#tasks");
 
 	form.addEventListener('submit', function(e) {
-		
+		e.preventDefault();
+
+		const task = input.value;
+
+		const task_el = document.createElement('div');
+		task_el.classList.add('task');
+
+		const task_content_el = document.createElement('div');
+		task_content_el.classList.add('content');
+
+		task_el.appendChild(task_content_el);
 	});
 });
