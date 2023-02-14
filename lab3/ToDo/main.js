0bb63e60c613796573a53aefe5b7712a722095aa
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-02-15 00:31:55 +0600

    Update main.js

--- lab3/ToDo/main.js
+++ lab3/ToDo/main.js
@@ -22,5 +22,24 @@ window.addEventListener('load', function() {
 		task_input_el.setAttribute('readonly', 'readonly');
 
 		task_content_el.appendChild(task_input_el);
+		const task_actions_el = document.createElement('div');
+		task_actions_el.classList.add('actions');
+		
+		const task_delete_el = document.createElement('button');
+		task_delete_el.classList.add('delete');
+		task_delete_el.innerText = 'Delete';
+
+		
+		task_actions_el.appendChild(task_delete_el);
+
+		task_el.appendChild(task_actions_el);
+
+		list_el.appendChild(task_el);
+
+		input.value = '';
+        
+        	task_delete_el.addEventListener('click', (e) => {
+			list_el.removeChild(task_el);
+		});
 	});
 });
