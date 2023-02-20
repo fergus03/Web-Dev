c8b3b8af4d5fa6db947a2e919b488f76e20c958c
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-02-20 11:35:46 +0600

    Update products.ts

--- lab4/src/app/products.ts
+++ lab4/src/app/products.ts
@@ -8,6 +8,24 @@ export interface Product {
 export const products = [
   {
     id: 1,
+    name: 'iPhone 11',
+    price: 296322,
+    rating: '4.8/5',
+    link: "https://kaspi.kz/shop/p/apple-iphone-11-128gb-slim-box-chernyi-100692388/?c=750000000#!/item",
+    image: "https://resources.cdn-kaspi.kz/shop/medias/sys_mas…1-128gb-slim-box-cernyj-100692388-1-Container.jpg",
+    description: "Смартфон Apple iPhone 11 128Gb Slim Box черный"
+  },
+  {
+    id: 2,
+    name: 'iPhone 11 Pro',
+    price: 599500,
+    rating: '4.9/5',
+    link: "https://kaspi.kz/shop/p/apple-iphone-11-pro-64gb-seryi-1005129/?c=750000000#!/item",
+    image: "https://resources.cdn-kaspi.kz/shop/medias/sys_mas…-iphone-11-pro-64gb-seryj-1005129-1-Container.jpg",
+    description: "Смартфон Apple iPhone 11 Pro 64Gb серый"
+  },
+  {
+    id: 3,
     name: 'iPhone 12',
     price: 323670,
     rating: '5/5',
@@ -16,7 +34,7 @@ export const products = [
     description: 'Смартфон Apple iPhone 12 64Gb черный'
   },
   {
-    id: 2,
+    id: 4,
     name: 'iPhone 12 Pro',
     price: 551000,
     rating: '4.8/5',
@@ -25,7 +43,7 @@ export const products = [
     description: 'Смартфон Apple iPhone 12 Pro 128Gb синий'
   },
   {
-    id: 3,
+    id: 5,
     name: 'iPhone 12 Pro Max',
     price: 666666,
     rating: '4.9/5',
@@ -34,7 +52,7 @@ export const products = [
     description: "Смартфон Apple iPhone 12 Pro Max 128Gb синий"
   },
   {
-    id: 4,
+    id: 6,
     name: 'iPhone 13',
     price: 384000,
     rating: '5/5',
@@ -43,7 +61,7 @@ export const products = [
     description: 'Смартфон Apple iPhone 13 128Gb черный'
   },
   {
-    id: 5,
+    id: 7,
     name: 'iPhone 13 Pro',
     price: 384000,
     rating: '5/5',
@@ -52,7 +70,7 @@ export const products = [
     description: "Смартфон Apple iPhone 13 128Gb черный"
   },
   {
-    id: 6,
+    id: 8,
     name: 'iPhone 13 Pro Max',
     price: 667130,
     rating: '5/5',
@@ -61,17 +79,31 @@ export const products = [
     description: "Смартфон Apple iPhone 13 Pro Max 128Gb зеленый"
   },
   {
-    id: 7,
+    id: 9,
     name: 'iPhone 14',
-    price: 299,
+    price: 558400,
     rating: '5/5',
     link:"https://kaspi.kz/shop/p/apple-iphone-14-128gb-goluboi-106363150/?c=750000000#!/item",
     image:'https://resources.cdn-kaspi.kz/shop/medias/sys_mas…630/apple-iphone-14-128gb-goluboj-106363150-1.jpg',
     description: 'Смартфон Apple iPhone 14 128Gb голубой'
   },
   {
-    id: 6,
-
+    id: 10,
+    name: 'iPhone 14 Pro',
+    price: 644206,
+    rating: '5/5',
+    link: "https://kaspi.kz/shop/p/apple-iphone-14-pro-256gb-fioletovyi-106363319/?c=750000000#!/item",
+    image: "https://resources.cdn-kaspi.kz/shop/medias/sys_mas…le-iphone-14-pro-128gb-fioletovyj-106363319-1.jpg",
+    description: "Смартфон Apple iPhone 14 Pro 256Gb фиолетовый"
+  },
+  {
+    id: 11,
+    name: 'iPhone 14 Pro Max',
+    price:701558,
+    rating: '4.8/5',
+    link: "https://kaspi.kz/shop/p/apple-iphone-14-pro-max-256gb-fioletovyi-106363342/?c=750000000#!/item",
+    image:"https://resources.cdn-kaspi.kz/shop/medias/sys_mas…phone-14-pro-max-128gb-fioletovyj-106363342-1.jpg",
+    description: "Смартфон Apple iPhone 14 Pro Max 256Gb фиолетовый"
   }
 ];
 
