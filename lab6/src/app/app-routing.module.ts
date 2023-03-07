2c30fc41e5a464e0addb633ac737e66a94b09f2c
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-03-08 00:08:17 +0600

    added path for album

--- lab6/src/app/app-routing.module.ts
+++ lab6/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {AboutComponent} from "./about/about.component";
 import {NotFoundComponent} from "./not-found/not-found.component";
+import {AlbumsComponent} from "./albums/albums.component";
 
 const routes: Routes = [
   {path: 'home', component: HomeComponent},
   {path: 'about', component: AboutComponent},
+  {path: 'albums', component: AlbumsComponent},
   {path: '', redirectTo: 'home', pathMatch: 'full'},
   {path: '**', component: NotFoundComponent}
 
