6774ca25d01710f2712661624ac0125b56c86217
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-03-08 01:49:51 +0600

    added url of albums id

--- lab6/src/app/albums.service.ts
+++ lab6/src/app/albums.service.ts
@@ -7,11 +7,14 @@ import {Album} from "./models";
   providedIn: 'root'
 })
 export class AlbumsService {
-  BASE_URL = 'https://jsonplaceholder.typicode.com'
+  BASE_URL = 'https://jsonplaceholder.typicode.com';
 
   constructor(private client: HttpClient) { }
 
   getAlbums(): Observable<Album[]>{
-    return this.client.get<Album[]>('https://jsonplaceholder.typicode.com/albums');
+    return this.client.get<Album[]>(`${this.BASE_URL}/albums`);
+  }
+  getAlbum(id: number): Observable<Album>{
+    return this.client.get<Album>(`${this.BASE_URL}/albums/${id}`)
   }
 }
