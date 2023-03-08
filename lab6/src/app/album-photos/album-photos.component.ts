ea9973f2608e1b8af35728ee686200788211f9a8
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-03-08 12:23:30 +0600

    added getalbumphoto from service, returnback method

--- lab6/src/app/album-photos/album-photos.component.ts
+++ lab6/src/app/album-photos/album-photos.component.ts
@@ -1,4 +1,9 @@
 import { Component } from '@angular/core';
+import { Album} from "../models";
+import {ActivatedRoute} from "@angular/router";
+import {Location} from "@angular/common";
+import {Photo} from "../models";
+import {AlbumsService} from "../albums.service";
 
 @Component({
   selector: 'app-album-photos',
@@ -6,5 +11,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./album-photos.component.css']
 })
 export class AlbumPhotosComponent {
+  album: Album;
+  loaded: boolean;
+  photos: Photo[];
 
+  constructor(private route: ActivatedRoute,
+              private location: Location,
+              private albumService: AlbumsService) {
+    this.album = {} as Album;
+    this.loaded = true;
+    this.photos = [];
+  }
+  ngOnInit(): void{
+    this.getAlbumPhotos();
+  }
+  getAlbumPhotos(): void{
+    this.route.paramMap.subscribe((params) => {
+      const id = Number(params.get('id'));
+      this.albumService.getAlbumPhotos(id).subscribe((photo) => {
+        this.photos = photo;
+        this.loaded = true;
+      })
+    })
+  }
+  returnBack(): void{
+    this.location.back();
+  }
 }
