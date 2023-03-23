a519a68f5b202ae7af3bebe87538b1539f4b85eb
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-03-23 17:32:35 +0600

    solved d, i problems

--- lab7/task1/informatics/loop/for/I.py
+++ lab7/task1/informatics/loop/for/I.py
@@ -1,6 +1,10 @@
 a = int(input())
 cnt = 0
-for i in range(1, a + 1):
+for i in range(1, int(a**0.5)):
     if a % i == 0:
-        cnt+=1
-print(cnt)#######################
+        cnt += 1
+
+if a % int(a**0.5) == 0:
+    print(cnt * 2 + 1)
+else:
+    print(cnt * 2)
