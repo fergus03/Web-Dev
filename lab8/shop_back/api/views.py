a76cde7b5ab8417661feb51aa022d5ac371c1162
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-04-03 17:33:31 +0600

    added product_list view to see list of products

--- lab8/shop_back/api/views.py
+++ lab8/shop_back/api/views.py
@@ -7,20 +7,4 @@ def products_list(request):
     products_json = [p.to_json() for p in products]
     return JsonResponse(products_json, safe=False, json_dumps_params={'indent':2})
 
-def product_detail(request, product_id):
-    product = Product.objects.get(id=product_id)
-    return JsonResponse(product.to_json(), json_dumps_params={'indent':2})
 
-def categories_list(request):
-    categories = Category.objects.all()
-    categories_json = [category.to_json() for category in categories]
-    return JsonResponse(categories_json, safe=False, json_dumps_params={'indent':2})
-
-def category_detail(request, category_id):
-    category = Category.objects.get(id=category_id)
-    return JsonResponse(category.to_json(), json_dumps_params={'indent':2})
-
-def category_products(request, category_id):
-    products = Product.objects.all().filter(categoryId=category_id)
-    products_json = [product.to_json() for product in products]
-    return JsonResponse(products_json, safe=False, json_dumps_params={'indent':2})
