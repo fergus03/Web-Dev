68c4534860f99cda35defa7895ef4a253e5b1bdb
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-04-03 17:34:02 +0600

    added product_detail view to see info ab 1product

--- lab8/shop_back/api/views.py
+++ lab8/shop_back/api/views.py
@@ -7,4 +7,7 @@ def products_list(request):
     products_json = [p.to_json() for p in products]
     return JsonResponse(products_json, safe=False, json_dumps_params={'indent':2})
 
+def product_detail(request, product_id):
+    product = Product.objects.get(id=product_id)
+    return JsonResponse(product.to_json(), json_dumps_params={'indent':2})
 
