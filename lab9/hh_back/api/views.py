bd254ad1b1d03057545d349443b649eced06ef40
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-04-09 14:57:14 +0600

    created view to see list of companies

--- lab9/hh_back/api/views.py
+++ lab9/hh_back/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http.response import JsonResponse
+from api.models import Vacancy, Company
 # Create your views here.
+def companies_list(request):
+    companies = Company.objects.all()
+    companies_json = [c.to_json() for c in companies]
+    return JsonResponse(companies_json, safe=False, json_dumps_params={'indent': 2})
