355440b12848ab1541f97a9d11b7f0b698f14732
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-04-09 14:57:41 +0600

    created view to see one company

--- lab9/hh_back/api/views.py
+++ lab9/hh_back/api/views.py
@@ -6,3 +6,10 @@ def companies_list(request):
     companies = Company.objects.all()
     companies_json = [c.to_json() for c in companies]
     return JsonResponse(companies_json, safe=False, json_dumps_params={'indent': 2})
+
+def company_detail(request, company_id):
+    try:
+        company = Company.objects.get(id=company_id)
+        return JsonResponse(company.to_json(), json_dumps_params={'indent': 2})
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': str(e)}, status=400)
