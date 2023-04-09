c213c868659bc696e481c0e7abc590715249aca6
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-04-09 15:04:55 +0600

    added decorators so it will look good

--- lab9/hh_back/api/admin.py
+++ lab9/hh_back/api/admin.py
@@ -2,6 +2,12 @@ from django.contrib import admin
 from api.models import Company, Vacancy
 
 # Register your models here.
-admin.site.register(Company)
-admin.site.register(Vacancy)
+# admin.site.register(Company)
+# admin.site.register(Vacancy)
+@admin.register(Company)
+class CompanyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'city')
+@admin.register(Vacancy)
+class VacancyAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'salary')
 
