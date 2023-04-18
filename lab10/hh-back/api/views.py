edadaa5e4d9ad2c050c9dbd2b2d8de48e9db987b
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-04-18 17:55:52 +0600

    created compnay list view with methods get, post

--- lab10/hh-back/api/views.py
+++ lab10/hh-back/api/views.py
@@ -1,3 +1,19 @@
+import json
+
+from django.views.decorators.csrf import csrf_exempt
+from django.http.response import HttpResponse, JsonResponse
+from .models import Company, Vacancy
+from .serializers import CompanySerializer, VacancySerializer
 from django.shortcuts import render
 
-# Create your views here.
+@csrf_exempt
+def company_list(request):
+    if request.method == 'GET':
+        companies = Company.objects.all()
+        companies_serialized = CompanySerializer(companies, many=True)
+        return JsonResponse(companies_serialized.data, safe=False)
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        company = Company.objects.create(name=data.get('name',''), description=data.get('description', ''),
+                                         city=data.get('city', ''), address=data.get('address', ''))
+        return JsonResponse(company.to_json(), safe=False)
