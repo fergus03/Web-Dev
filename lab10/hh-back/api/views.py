84dffb7c3e771f68c8bd993ad668742ce1536245
fergus03 <120181692+fergus03@users.noreply.github.com>
2023-04-18 17:57:03 +0600

    created company_detail view with get, put, delete

--- lab10/hh-back/api/views.py
+++ lab10/hh-back/api/views.py
@@ -17,3 +17,23 @@ def company_list(request):
         company = Company.objects.create(name=data.get('name',''), description=data.get('description', ''),
                                          city=data.get('city', ''), address=data.get('address', ''))
         return JsonResponse(company.to_json(), safe=False)
+
+@csrf_exempt
+def company_detail(request, id):
+    try:
+        company = Company.objects.get(id=id)
+    except Company.DoesNotExist as e:
+        return JsonResponse({'error': str(e)})
+    if request.method == 'GET':
+        return JsonResponse(company.to_json(), safe=False)
+    elif request.method == 'PUT':
+        data = json.loads(request.body)
+        company.name = data.get('name', company.name)
+        company.description = data.get('description', company.description)
+        company.city = data.get('city', company.city)
+        company.address = data.get('address', company.address)
+        company.save()
+        return JsonResponse(company.to_json(), safe=False)
+    elif request.method == 'DELETE':
+        company.delete()
+        return JsonResponse({'deleted': True})
